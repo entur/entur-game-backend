1c78d34cb76ab0fe6f3e26e93dc74ff4b053cbc8
Daniel Jansson <daniel.jansson@entur.org>
2023-04-19 19:38:54 +0200

    Added a timeconverter from seconds to hh:mm:ss

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
@@ -1,6 +1,7 @@
 package org.entur.norgesturbackend.norgestur.controller
 
-import org.entur.norgesturbackend.norgestur.model.PlayerScore
+import org.entur.norgesturbackend.norgestur.model.PlayerScoreDto
+import org.entur.norgesturbackend.norgestur.model.toResponse
 import org.entur.norgesturbackend.norgestur.service.PlayerScoreService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class PlayerScoreController (val playerScoreService: PlayerScoreService){
     @GetMapping("/player-score")
-    fun getPlayerScore(): List<PlayerScore> {
-        return playerScoreService.getAll()
+    fun getPlayerScore(): List<PlayerScoreDto> {
+        return playerScoreService.getAll().map { it.toResponse() }
     }
 }
--- src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
@@ -2,7 +2,6 @@ package org.entur.norgesturbackend.norgestur.model
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
-import java.sql.Time
 
 @Entity
 data class PlayerScore(
@@ -16,3 +15,33 @@ data class PlayerScore(
         val fromDestination: String,
         val toDestination: String
 )
+
+data class PlayerScoreDto(
+        val nickname: String,
+        val score: Int,
+        val totalOptions: Int,
+        val totalPlaytime: String,
+        val totalTravelTime: String,
+        val fromDestination: String,
+        val toDestination: String
+)
+
+fun PlayerScore.toResponse(): PlayerScoreDto {
+       val minutesPlayTime = totalPlaytime/60
+       val secondsPlayTime = totalPlaytime%60
+       val totalPT = "$minutesPlayTime:$secondsPlayTime"
+        val hoursTravelTime = totalTravelTime/3600
+        val minutesTravelTime = (totalTravelTime % 3600)/60
+        val secondsTravelTime = totalTravelTime%60
+        val totalTT = "$hoursTravelTime:$minutesTravelTime:$secondsTravelTime"
+        return PlayerScoreDto(
+                nickname = nickname,
+                score = score,
+                totalOptions = totalOptions,
+                totalPlaytime = totalPT,
+                totalTravelTime = totalTT,
+                fromDestination = fromDestination,
+                toDestination = toDestination
+        )
+
+}
