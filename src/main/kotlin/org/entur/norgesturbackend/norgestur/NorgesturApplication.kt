9a07e095e511ab589e288e497cc17593e828bcaa
Daniel Jansson <daniel.jansson@entur.org>
2023-04-19 14:08:38 +0200

    initial project

---
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/NorgesturApplication.kt
@@ -0,0 +1,11 @@
+package org.entur.norgesturbackend.norgestur
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NorgesturApplication
+
+fun main(args: Array<String>) {
+	runApplication<NorgesturApplication>(*args)
+}
---
+++ src/test/kotlin/org/entur/norgesturbackend/norgestur/NorgesturApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.entur.norgesturbackend.norgestur
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NorgesturApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
