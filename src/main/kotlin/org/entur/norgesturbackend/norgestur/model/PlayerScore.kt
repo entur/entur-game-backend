d2ce63bda44ba775ca2f9eca8a33ff9134f75e06
Daniel Jansson <daniel.jansson@entur.org>
2023-04-20 10:38:56 +0200

    new score

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
@@ -37,9 +37,13 @@ fun PlayerScore.toResponse(): PlayerScoreDto {
         val minutesTravelTime = (totalTravelTime % 3600)/60
         val secondsTravelTime = totalTravelTime%60
         val totalTT = "$hoursTravelTime:$minutesTravelTime:$secondsTravelTime"
+        val factor1 = 0.6
+        val factor2 = 0.1
+        val factor3 = 0.4
+        val playerScore = (factor1 * totalOptions) + (factor2 * totalPlaytime) + (factor3 * totalTravelTime)
         return PlayerScoreDto(
                 nickname = nickname,
-                score = score,
+                score = playerScore.toInt(),
                 totalOptions = totalOptions,
                 totalPlaytime = totalPT,
                 totalTravelTime = totalTT,
