c0f7e908aac3bf2be49d579077bc9b6a2e92da7a
Daniel Jansson <daniel.jansson@entur.org>
2023-04-20 09:13:53 +0200

    Post request for playerscore

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
@@ -1,9 +1,12 @@
 package org.entur.norgesturbackend.norgestur.controller
 
+import org.entur.norgesturbackend.norgestur.model.PlayerScore
 import org.entur.norgesturbackend.norgestur.model.PlayerScoreDto
 import org.entur.norgesturbackend.norgestur.model.toResponse
 import org.entur.norgesturbackend.norgestur.service.PlayerScoreService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,5 +14,11 @@ class PlayerScoreController (val playerScoreService: PlayerScoreService){
     @GetMapping("/player-score")
     fun getPlayerScore(): List<PlayerScoreDto> {
         return playerScoreService.getAll().map { it.toResponse() }
+    }
+    @PostMapping("/player-score")
+    fun addPlayerScore(
+            @RequestBody playerScore: PlayerScore
+    ): PlayerScore{
+        return playerScoreService.savePlayerScore(playerScore)
     }
 }
