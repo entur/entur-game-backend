b94ef2036aba434ff1a2b69c7b7cc99b2c69d5f3
Daniel Jansson <daniel.jansson@entur.org>
2023-04-20 13:31:41 +0200

    Moved logic from controller to service

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
@@ -8,10 +8,10 @@ data class PlayerScore(
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Int,
         val nickname: String,
-        val score: Int,
+        var score: Int,
         val totalOptions: Int,
-        val totalPlaytime: Int,
-        val totalTravelTime: Int,
+        var totalPlaytime: String,
+        var totalTravelTime: String,
         @ManyToOne
         @JoinColumn(name = "from_destination", referencedColumnName = "id")
         val fromDestination: Destination,
@@ -31,57 +31,12 @@ data class PlayerScoreDto(
 )
 
 fun PlayerScore.toResponse(): PlayerScoreDto {
-        val hoursPlayTime = totalPlaytime/3600
-       val minutesPlayTime = (totalPlaytime % 3600)/60
-       val secondsPlayTime = totalPlaytime%60
-
-        val hoursTravelTime = totalTravelTime/3600
-        val minutesTravelTime = (totalTravelTime % 3600)/60
-        val secondsTravelTime = totalTravelTime%60
-
-    val totalHoursPlayed: String = if (hoursPlayTime <= 9){
-        "0$hoursPlayTime"
-    } else {
-        hoursPlayTime.toString()
-    }
-    val totalMinutesPlayed: String = if (minutesPlayTime <= 9){
-        "0$minutesPlayTime"
-    } else {
-        minutesPlayTime.toString()
-    }
-    val totalSecondsPlayed: String = if (secondsPlayTime <= 9){
-        "0$secondsPlayTime"
-    } else {
-        secondsPlayTime.toString()
-    }
-
-    val totalHoursTraveled: String = if (hoursTravelTime <= 9){
-    "0$hoursTravelTime"
-} else {
-    hoursTravelTime.toString()
-}
-    val totalMinutesTraveled: String = if (minutesTravelTime <= 9){
-        "0$minutesTravelTime"
-    } else {
-        minutesTravelTime.toString()
-    }
-    val totalSecondsTraveled: String = if (secondsTravelTime <= 9){
-        "0$secondsTravelTime"
-    } else {
-        secondsTravelTime.toString()
-    }
-        val totalPT = "$totalHoursPlayed:$totalMinutesPlayed:$totalSecondsPlayed"
-        val totalTT = "$totalHoursTraveled:$totalMinutesTraveled:$totalSecondsTraveled"
-        val factor1 = 0.6
-        val factor2 = 0.1
-        val factor3 = 0.4
-        val playerScore = (factor1 * totalOptions) + (factor2 * totalPlaytime) + (factor3 * totalTravelTime)
         return PlayerScoreDto(
                 nickname = nickname,
-                score = playerScore.toInt(),
+                score = score,
                 totalOptions = totalOptions,
-                totalPlaytime = totalPT,
-                totalTravelTime = totalTT,
+                totalPlaytime = totalPlaytime,
+                totalTravelTime = totalTravelTime,
                 fromDestination = fromDestination,
                 toDestination = toDestination
         )
--- src/main/kotlin/org/entur/norgesturbackend/norgestur/service/PlayerScoreService.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/service/PlayerScoreService.kt
@@ -11,6 +11,53 @@ class PlayerScoreService (val playerScoreRepository : PlayerScoreRepository){
     }
 
     fun savePlayerScore(playerScore: PlayerScore): PlayerScore{
+
+        val hoursPlayTime = playerScore.totalPlaytime.toInt()/3600
+        val minutesPlayTime = (playerScore.totalPlaytime.toInt() % 3600)/60
+        val secondsPlayTime = playerScore.totalPlaytime.toInt()%60
+
+        val hoursTravelTime = playerScore.totalTravelTime.toInt()/3600
+        val minutesTravelTime = (playerScore.totalTravelTime.toInt() % 3600)/60
+        val secondsTravelTime = playerScore.totalTravelTime.toInt()%60
+
+        val totalHoursPlayed: String = if (hoursPlayTime <= 9){
+            "0$hoursPlayTime"
+        } else {
+            hoursPlayTime.toString()
+        }
+        val totalMinutesPlayed: String = if (minutesPlayTime <= 9){
+            "0$minutesPlayTime"
+        } else {
+            minutesPlayTime.toString()
+        }
+        val totalSecondsPlayed: String = if (secondsPlayTime <= 9){
+            "0$secondsPlayTime"
+        } else {
+            secondsPlayTime.toString()
+        }
+
+        val totalHoursTraveled: String = if (hoursTravelTime <= 9){
+            "0$hoursTravelTime"
+        } else {
+            hoursTravelTime.toString()
+        }
+        val totalMinutesTraveled: String = if (minutesTravelTime <= 9){
+            "0$minutesTravelTime"
+        } else {
+            minutesTravelTime.toString()
+        }
+        val totalSecondsTraveled: String = if (secondsTravelTime <= 9){
+            "0$secondsTravelTime"
+        } else {
+            secondsTravelTime.toString()
+        }
+
+        val factor1 = 0.6
+        val factor2 = 0.1
+        val factor3 = 0.4
+        playerScore.score = ((factor1 * playerScore.totalOptions) + (factor2 * playerScore.totalPlaytime.toInt()) + (factor3 * playerScore.totalTravelTime.toInt())).toInt()
+        playerScore.totalPlaytime = "$totalHoursPlayed:$totalMinutesPlayed:$totalSecondsPlayed"
+        playerScore.totalTravelTime = "$totalHoursTraveled:$totalMinutesTraveled:$totalSecondsTraveled"
         return playerScoreRepository.save(playerScore)
     }
 }
