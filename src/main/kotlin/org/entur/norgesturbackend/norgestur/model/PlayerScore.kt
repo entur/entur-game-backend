87c9a33e0c4eadaf5f33853e5bad5dc51c4155cc
Daniel Jansson <daniel.jansson@entur.org>
2023-04-20 11:48:11 +0200

    Added 0 to time values less than 10

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/model/PlayerScore.kt
@@ -31,13 +31,47 @@ data class PlayerScoreDto(
 )
 
 fun PlayerScore.toResponse(): PlayerScoreDto {
-       val minutesPlayTime = totalPlaytime/60
+        val hoursPlayTime = totalPlaytime/3600
+       val minutesPlayTime = (totalPlaytime % 3600)/60
        val secondsPlayTime = totalPlaytime%60
-       val totalPT = "$minutesPlayTime:$secondsPlayTime"
+
         val hoursTravelTime = totalTravelTime/3600
         val minutesTravelTime = (totalTravelTime % 3600)/60
         val secondsTravelTime = totalTravelTime%60
-        val totalTT = "$hoursTravelTime:$minutesTravelTime:$secondsTravelTime"
+
+    val totalHoursPlayed: String = if (hoursPlayTime <= 9){
+        "0$hoursPlayTime"
+    } else {
+        hoursPlayTime.toString()
+    }
+    val totalMinutesPlayed: String = if (minutesPlayTime <= 9){
+        "0$minutesPlayTime"
+    } else {
+        minutesPlayTime.toString()
+    }
+    val totalSecondsPlayed: String = if (secondsPlayTime <= 9){
+        "0$secondsPlayTime"
+    } else {
+        secondsPlayTime.toString()
+    }
+
+    val totalHoursTraveled: String = if (hoursTravelTime <= 9){
+    "0$hoursTravelTime"
+} else {
+    hoursTravelTime.toString()
+}
+    val totalMinutesTraveled: String = if (minutesTravelTime <= 9){
+        "0$minutesTravelTime"
+    } else {
+        minutesTravelTime.toString()
+    }
+    val totalSecondsTraveled: String = if (secondsTravelTime <= 9){
+        "0$secondsTravelTime"
+    } else {
+        secondsTravelTime.toString()
+    }
+        val totalPT = "$totalHoursPlayed:$totalMinutesPlayed:$totalSecondsPlayed"
+        val totalTT = "$totalHoursTraveled:$totalMinutesTraveled:$totalSecondsTraveled"
         val factor1 = 0.6
         val factor2 = 0.1
         val factor3 = 0.4
