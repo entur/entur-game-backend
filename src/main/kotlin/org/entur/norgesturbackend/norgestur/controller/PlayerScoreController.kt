c7b945e4f20b90e9260380a3944f3914feede095
Daniel Jansson <daniel.jansson@entur.org>
2023-05-05 13:51:02 +0200

    Added difficulty to player-score

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/PlayerScoreController.kt
@@ -7,11 +7,18 @@ import org.entur.norgesturbackend.norgestur.service.PlayerScoreService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PlayerScoreController (val playerScoreService: PlayerScoreService){
     @GetMapping("/player-score")
+    fun getPlayerScoreByDifficulty(
+            @RequestParam difficulty: String,
+    ): List<PlayerScoreDto> {
+        return playerScoreService.getTopTenByDifficulty(difficulty.lowercase()).map { it.toResponse() }
+    }
+    @GetMapping("/player-score/top-ten-overall")
     fun getPlayerScore(): List<PlayerScoreDto> {
         return playerScoreService.getTopTen().map { it.toResponse() }
     }
--- src/main/kotlin/org/entur/norgesturbackend/norgestur/repository/PlayerScoreRepository.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/repository/PlayerScoreRepository.kt
@@ -3,8 +3,16 @@ package org.entur.norgesturbackend.norgestur.repository
 import org.entur.norgesturbackend.norgestur.model.PlayerScore
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface PlayerScoreRepository : JpaRepository<PlayerScore, Int>{
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM player_score WHERE difficulty = (:difficulty) ORDER BY score DESC LIMIT 10"
+    )
+    fun findTopTenScoresByDifficulty(
+            @Param("difficulty") difficulty: String
+    ): List<PlayerScore>
     @Query(
             nativeQuery = true,
             value = "SELECT * FROM player_score ORDER BY score DESC LIMIT 10"
