f4980e1c232bb2d620d51e37ddddcc54e7023c7e
Daniel Jansson <daniel.jansson@entur.org>
2023-09-01 14:13:55 +0200

    FEATURE: Secure backend straight into prod

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-configuration-processor")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework:spring-websocket:6.0.8")
