bd7f5f1fb1c8b33b50fb20392d89c925e7c0fdb4
Kenneth Ngo <kenneth.ngo@entur.org>
2023-09-05 20:08:30 +0200

    fix norwegian letters conversion

--- src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/GameModeController.kt
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/controller/GameModeController.kt
@@ -3,17 +3,18 @@ package org.entur.norgesturbackend.norgestur.controller
 import org.entur.norgesturbackend.norgestur.config.MyProperties
 import org.entur.norgesturbackend.norgestur.model.GameMode
 import org.entur.norgesturbackend.norgestur.service.GameModeService
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GameModeController (val gameModeService: GameModeService, val myProperties: MyProperties) {
+class GameModeController(val gameModeService: GameModeService, val myProperties: MyProperties) {
 
 
     @GetMapping("/game-mode/{difficulty}")
@@ -25,16 +26,18 @@ class GameModeController (val gameModeService: GameModeService, val myProperties
 
     @GetMapping("/game-mode/optimal-route/{difficulty}")
     fun getOptimalRouteText(
-        @PathVariable difficulty: String
-    ): String{
-        return gameModeService.getOptimalRouteText(difficulty)
+        @PathVariable difficulty: String,
+    ): ResponseEntity<String> {
+        val responseHeaders = HttpHeaders()
+        responseHeaders["Content-Type"] = "text/plain;charset=utf-8"
+        return ResponseEntity.ok().headers(responseHeaders).body(gameModeService.getOptimalRouteText(difficulty))
     }
 
     @PostMapping("game-mode")
     fun addGameMode(
         @RequestBody gameMode: GameMode,
         @RequestHeader("Auth") secret: String
-    ): HttpStatus{
+    ): HttpStatus {
         if (secret != myProperties.secret) return HttpStatus.FORBIDDEN
 
         return gameModeService.saveGameMode(gameMode)
