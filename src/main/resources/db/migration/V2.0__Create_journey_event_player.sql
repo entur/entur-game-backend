8665ef268a685a55aa20d167629b1e26b1f2d4c6
Oscar Aleksander Halvorsen <oscar@bekk-mac-2281.shared>
2024-07-01 16:06:35 +0200

    changed db to new db structure

--- src/main/resources/db/migration/V2.0__Create_journey_event_player.sql
+++ src/main/resources/db/migration/V2.0__Create_journey_event_player.sql
@@ -1,29 +1,28 @@
-CREATE TABLE JOURNEY (
-                         journeyId serial PRIMARY KEY,
-                         journeyName VARCHAR(100) NOT NULL,
-                         startLocationId VARCHAR(100) NOT NULL,
-                         endLocationId VARCHAR(100) NOT NULL,
-                         startTime TIMESTAMP NOT NULL,
-                         optimalStepNr INT NOT NULL,
-                         optimalTravelTime INT NOT NULL
-);
-
 CREATE TABLE EVENT (
-                       eventId serial PRIMARY KEY,
-                       eventName VARCHAR(100) NOT NULL,
-                       journeyId INT NOT NULL,
-                       FOREIGN KEY (journeyId) REFERENCES JOURNEY(journeyId)
+    eventId serial PRIMARY KEY,
+    eventName VARCHAR(100) NOT NULL,
+    startLocationId VARCHAR(100) NOT NULL,
+    endLocationId VARCHAR(100) NOT NULL,
+    startTime TIMESTAMP NOT NULL,
+    optimalStepNumber INT NOT NULL,
+    optimalTravelTime INT NOT NULL
 );
 
 CREATE TABLE PLAYER (
-                        playerId serial PRIMARY KEY,
-                        playerName VARCHAR(30) NOT NULL,
-                        score INT,
-                        totalStepNr INT,
-                        totalTravelTime INT,
-                        totalPlayTime INT,
-                        email VARCHAR(255) NOT NULL,
-                        phoneNr VARCHAR(16) NOT NULL, -- varchar because of leading zeroes and plus sign
-                        eventId INT,
-                        FOREIGN KEY (eventId) REFERENCES EVENT(eventId)
+    playerId serial PRIMARY KEY,
+    playerName VARCHAR(30) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(16) NOT NULL UNIQUE -- varchar because of leading zeroes and plus sign
+);
+
+CREATE TABLE SCORE (
+    scoreId serial PRIMARY KEY,
+    scoreValue INT NOT NULL,
+    totalStepNumber INT NOT NULL,
+    totalTravelTime INT NOT NULL,
+    totalPlayTime INT NOT NULL,
+    eventId INT NOT NULL,
+    playerId INT NOT NULL,
+    FOREIGN KEY (eventId) REFERENCES EVENT(eventId),
+    FOREIGN KEY (playerId) REFERENCES PLAYER(playerId)
 );
--- src/main/resources/db/migration/V2.1__Insert_journey_event_player.sql
+++ src/main/resources/db/migration/V2.1__Insert_journey_event_player.sql
@@ -1,20 +1,26 @@
--- Inserting data into the JOURNEY table
-INSERT INTO JOURNEY (journeyName, startLocationId, endLocationId, startTime, optimalStepNr, optimalTravelTime)
-VALUES
-    ('Journey 1: easy', 'NSR:StopPlace:58366', 'NSR:StopPlace:59977', '2024-06-01 10:00:00', 2, 34020),
-    ('Journey 2: middle', 'NSR:StopPlace:22329', 'NSR:StopPlace:9625', '2024-06-01 11:00:00', 6, 57780),
-    ('Journey 3: hard', 'NSR:StopPlace:58366', 'NSR:StopPlace:198', '2024-06-01 12:00:00', 7, 123000);
-
 -- Inserting data into the EVENT table
-INSERT INTO EVENT (eventName, journeyId)
+INSERT INTO EVENT (eventName, startLocationId, endLocationId, startTime, optimalStepNumber, optimalTravelTime)
 VALUES
-    ('Event 1', 1),
-    ('Event 2', 2),
-    ('Event 3', 3);
+    ('Event 1: easy', 'NSR:StopPlace:58366', 'NSR:StopPlace:59977', '2024-06-01 10:00:00', 2, 34020),
+    ('Event 2: middle', 'NSR:StopPlace:22329', 'NSR:StopPlace:9625', '2024-06-01 11:00:00', 6, 57780),
+    ('Event 3: hard', 'NSR:StopPlace:58366', 'NSR:StopPlace:198', '2024-06-01 12:00:00', 7, 123000);
 
 -- Inserting data into the PLAYER table
-INSERT INTO PLAYER (playerName, score, totalStepNr, totalTravelTime, totalPlayTime, email, phoneNr, eventId)
+INSERT INTO PLAYER (playerName, email, phoneNumber)
 VALUES
-    ('Player 1', 100, 2, 1200, 120, 'player1@example.com', '12345678', 1),
-    ('Player 2', 70, 3, 1800, 180, 'player2@example.com', '09876543', 2),
-    ('Player 3', 50, 4, 2400, 240, 'player3@example.com', '11223344', 3);
+    ('Player 1', 'player1@example.com', '12345678'),
+    ('Player 2', 'player2@example.com', '09876543'),
+    ('Player 3', 'player3@example.com', '11223344');
+
+-- Inserting data into the SCORE table
+INSERT INTO SCORE (scoreValue, totalStepNumber, totalTravelTime, totalPlayTime, eventId, playerId)
+VALUES
+    (100, 2, 34020, 100, 1, 1),
+    (70, 3, 340200, 150, 1, 2),
+    (50, 4, 3402000, 200, 1, 3),
+    (100, 6, 57780, 100, 2, 1),
+    (70, 7, 577800, 150, 2, 2),
+    (50, 8, 5778000, 200, 2, 3),
+    (100, 7, 123000, 100, 3, 1),
+    (70, 8, 1230000, 150, 3, 2),
+    (50, 9, 12300000, 200, 3, 3);
