70261f6f239f5e1bd67465ec3239af1fc82246f0
Magnus Rosvold Farstad <magnus.r.farstad@outlook.com>
2024-07-30 12:23:18 +0200

    reintroduced web config

---
+++ src/main/kotlin/org/entur/norgesturbackend/norgestur/config/WebConfig.kt
@@ -0,0 +1,25 @@
+package org.entur.norgesturbackend.norgestur.config
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean? {
+        val container = ServletServerContainerFactoryBean()
+        container.setMaxSessionIdleTimeout(600000L) // set the timeout to 10min
+        return container
+    }
+}
